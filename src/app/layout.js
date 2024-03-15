76e27a1deb9598c7e1ed2c7e2e6eb72f2efa8ef7
Dirghayu Raichura <dirghayuraichura@gmail.com>
2024-03-15 21:19:42 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Providers } from "./provider";
+
+
 
 export const metadata = {
   title: "Create Next App",
@@ -10,8 +13,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="scroll-smooth">
+      <body>
+        <div className="dark text-foreground bg-background">
+        <Providers>
+        <Header />
+          <main >
+            {children}
+          </main>
+          <Footer />
+        </Providers>
+        </div>
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+import { Montserrat as montserrat,Macondo as mocando } from 'next/font/google';
+
+
+import {nextui} from "@nextui-org/react";
+import { config } from "next/dist/build/templates/pages";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -12,7 +19,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily:{
+        montserrat : ["var(--font-montserrat)"],
+        macando : ["var(--font-macando)"]
+      }
     },
   },
-  plugins: [],
+  plugins: [nextui()]
 };
+
